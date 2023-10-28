01d321b6a1eb5b345e97d8d21ee8edd69fbb8900
PriyanshGupta2002 <guptapriyansh23@gmail.com>
2023-10-28 16:58:05 +0530

    fixes

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,11 +1,47 @@
+// import { getServerSession } from "next-auth/next";
+
+// import primsaClient from "../utils/prismaClient";
+// import { authOptions } from "../api/auth/[...nextauth]/route";
+
+// const getSession = async () => {
+//   const session = await getServerSession(authOptions);
+//   return session;
+// };
+
+// export const getCurrentUser = async () => {
+//   try {
+//     const session = await getSession();
+//     if (!session?.user?.email) {
+//       return null;
+//     }
+//     const currentUser = await primsaClient.user.findUnique({
+//       where: {
+//         email: session.user.email,
+//       },
+//       include: {
+//         Projects: true,
+//       },
+//     });
+//     if (!currentUser) {
+//       return null;
+//     }
+//     return {
+//       ...currentUser,
+//       createdAt: currentUser.createdAt.toISOString(),
+//       updatedAt: currentUser.updatedAt.toISOString(),
+//       emailVerified: currentUser.emailVerified?.toISOString(),
+//     };
+//   } catch (error) {
+//     return null;
+//   }
+// };
+
 import { getServerSession } from "next-auth/next";
-
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import primsaClient from "../utils/prismaClient";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 
-const getSession = async () => {
-  const session = await getServerSession(authOptions);
-  return session;
+export const getSession = async () => {
+  return await getServerSession(authOptions);
 };
 
 export const getCurrentUser = async () => {
@@ -16,12 +52,10 @@ export const getCurrentUser = async () => {
     }
     const currentUser = await primsaClient.user.findUnique({
       where: {
-        email: session.user.email,
-      },
-      include: {
-        Projects: true,
+        email: session.user.email as string,
       },
     });
+
     if (!currentUser) {
       return null;
     }
@@ -29,7 +63,7 @@ export const getCurrentUser = async () => {
       ...currentUser,
       createdAt: currentUser.createdAt.toISOString(),
       updatedAt: currentUser.updatedAt.toISOString(),
-      emailVerified: currentUser.emailVerified?.toISOString(),
+      emailVerified: currentUser.emailVerified?.toISOString() || null,
     };
   } catch (error) {
     return null;

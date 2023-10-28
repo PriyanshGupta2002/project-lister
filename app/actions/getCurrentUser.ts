fd55d9932c1fa1de4a95e5eeff2e362159ff475d
PriyanshGupta2002 <guptapriyansh23@gmail.com>
2023-10-28 16:22:34 +0530

    fixes

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+
 import primsaClient from "../utils/prismaClient";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 const getSession = async () => {
   const session = await getServerSession(authOptions);
   return session;
